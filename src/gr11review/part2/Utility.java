1c4a3c0abd9b2cdc241f92119412ef9bb9881d44
James Bian <james.bian23@ycdsbk12.ca>
2022-09-29 22:18:48 -0400

    First version of all questions

--- src/gr11review/part2/Utility.java
+++ src/gr11review/part2/Utility.java
@@ -1,6 +1,7 @@
 package gr11review.part2;
 
 import java.io.*;
+import java.util.*;
 
 public class Utility {
 
@@ -40,9 +41,12 @@ public class Utility {
     }
 
     public static int vowelInLine(String line) {
+        // Function to count vowels in a word every loop
+
         String vowels = "aeiou";
         int vowelCount = 0;
         for (int i = 0; i < line.length(); i++) {
+
             if (vowels.indexOf(line.charAt(i)) >= 0) {
                 vowelCount++;
 
@@ -55,8 +59,13 @@ public class Utility {
         String mostVowelWord = "";
         int maxVowel = 0;
         BufferedReader input = new BufferedReader(new FileReader(filenametxt));
-
         String line = input.readLine();
+
+        /*
+         * While there is a character in line, loop will call vowelInLine for each
+         * String
+         * Variables maxVowel and mostVowelWord keep track of the word with most vowels
+         */
         while (line != null) {
 
             System.out.println(line);
@@ -70,5 +79,68 @@ public class Utility {
         }
         input.close();
         return mostVowelWord;
+    }
+
+    public static int[] withoutTen(int[] nums) {
+
+        /*
+         * Checks for numbers = 10 throughout array
+         * If the if statement is met, the int will be turned to 0, and moved to last
+         * characters of array
+         */
+        // intial value of the element of output array is 0
+        int[] outNums = new int[nums.length];
+        int outArrIndex = 0;
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] != 10) {
+                outNums[outArrIndex] = nums[i];
+                outArrIndex++;
+
+            }
+        }
+
+        return outNums;
+    }
+
+    public static boolean linearIn(int[] outer, int[] inner) {
+        boolean foundAll = false;
+        int innerIndex = 0;
+        for (int i = 0; i < outer.length; i++) {
+            if (outer[i] == inner[innerIndex]) {
+                innerIndex++;
+                if (innerIndex >= inner.length) {
+                    foundAll = true;
+                    break;
+                }
+            }
+        }
+
+        return foundAll;
+    }
+
+    public static int[][] reverse(int[][] arr) {
+        int rowCount = arr.length;
+        int colCount = arr[0].length;
+        int reverseArr[][] = new int[rowCount][colCount];
+
+        for (int i = 0; i < rowCount; i++) {
+            for (int j = 0; j < colCount; j++) {
+                 reverseArr[rowCount - i - 1][colCount - j - 1] = arr [i][j] ;
+            }
+        }
+
+        return arr;
+    }
+
+    public static void main(String[] args) {
+        int[][] numbers = {
+                { 1, 2, 3 },
+                { 4, 5, 6 },
+                { 7, 8, 9 },
+        };
+
+        int[][] outNums = Utility.reverse(numbers);
+        System.out.println(outNums);
+
     }
 }
--- src/gr11review/part2/UtilityTest.java
+++ src/gr11review/part2/UtilityTest.java
@@ -6,54 +6,85 @@ import java.io.IOException;
 
 import org.junit.jupiter.api.Test;
 
-//import gr11review.part2.Utility;
+import gr11review.part2.Utility;
 
 public class UtilityTest{
 
     @Test
+    // Testing simple case
     void xyBalanceTest1(){
         assertEquals(true, Utility.xyBalance("abxyc"));
     }
 
     @Test
+    // Testing case with no x
     void xyBalanceTest2(){
-        assertEquals(true, Utility.xyBalance("xxy"));
-    }
-
-    @Test
-    void xyBalanceTest3(){
         assertEquals(true, Utility.xyBalance("acbd"));
-    }
-
-    @Test
-    void xyBalanceTest4(){
-        assertEquals(true, Utility.xyBalance("a"));
     }
     
     @Test
-    void xyBalanceTest5(){
+    // Testing y appearing before x
+    void xyBalanceTest3(){
         assertEquals(false, Utility.xyBalance("abyxc"));
     }
 
     @Test
-    void xyBalanceTest6(){
-        assertEquals(true, Utility.xyBalance("abyc"));
-    }
-    @Test
-    void xyBalanceTest7(){
-        assertEquals(true, Utility.xyBalance("bwxxuuuuuuuy"));
-    }
-    @Test
-    void xyBalanceTest8(){
+    // Testing false case with only x
+    void xyBalanceTest4(){
         assertEquals(false, Utility.xyBalance("xxxx"));
     }
     @Test
-    void xyBalanceTest9(){
+    // Testing Simple case with no x
+    void xyBalanceTest5(){
         assertEquals(false, Utility.xyBalance("abxde"));
     }
+
+
     @Test
-    void vowelCountTest1() throws IOException{
+    // Testing with Review text 1
+    void vowelCountTest1() throws IOException {
         assertEquals("interactive",Utility.vowelCount("C:\\Users\\buyat\\grade-11-review-2---methods-and-testing-james-brighton-hayden-1\\src\\gr11review\\test2\\Review2_3Test_1.txt"));
     }
+    @Test
+    // Testing with Review text 2
+    void vowelCountTest2() throws IOException {
+        assertEquals("poisonous",Utility.vowelCount("C:\\Users\\buyat\\grade-11-review-2---methods-and-testing-james-brighton-hayden-1\\src\\gr11review\\test2\\Review2_3Test_2.txt"));
+    }
+    @Test
+    // Testing with Review text 3
+    void vowelCountTest3() throws IOException {
+        assertEquals("visualization",Utility.vowelCount("C:\\Users\\buyat\\grade-11-review-2---methods-and-testing-james-brighton-hayden-1\\src\\gr11review\\test2\\Review2_3Test_3.txt"));
+    }
+    @Test
+    // Testing with Review text 4
+    void vowelCountTest4() throws IOException {
+        assertEquals("liberation",Utility.vowelCount("C:\\Users\\buyat\\grade-11-review-2---methods-and-testing-james-brighton-hayden-1\\src\\gr11review\\test2\\Review2_3Test_4.txt"));
+    }
 
+
+    @Test
+    //Simple case of true
+    void linearIntest1(){
+        assertTrue(Utility.linearIn(new int[]{1,3,5,7,9}, new int[]{5,7}));
+    }
+
+    @Test
+    // Testing case with identical arrays
+    void linearIntest2(){
+        assertTrue((Utility.linearIn(new int[]{1,3,5,7,9}, new int[]{1,3,5,7,9})));
+    }
+
+    @Test
+    // Testing simple false case
+    void linearIntest3(){
+        assertFalse(Utility.linearIn(new int[]{1,3,5,7,9}, new int[]{2,0}));
+    }
+
+    @Test
+    // Testing one variable
+    void linearIntest4(){
+        assertTrue(Utility.linearIn(new int[]{1}, new int[]{1}));
+    }
+
+    
 }
