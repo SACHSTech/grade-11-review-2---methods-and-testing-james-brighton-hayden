95d6dbc4a8bb9db1d8cc463dcf7ce913e1b26df1
BrightonZhang1 <brighton.zhang23@ycdsbk12.ca>
2022-10-03 18:56:12 -0400

    Finished

--- src/gr11review/part2/Utility.java
+++ src/gr11review/part2/Utility.java
@@ -1,8 +1,145 @@
 package gr11review.part2;
 
+import java.io.*;
+import java.util.ArrayList;
+
 public class Utility {
 
-    
+    /**
+     * Given a string, determine the sum of the numbers appearing in the string
+     * 
+     * @param str String given to determine the sum
+     * @return intSum (sum of numbers) back to user
+     * @author B. Zhang
+     */
+    public static int sumNumbers(String str){
+        String strNum = "";
+        int intSum = 0;
 
+        // For loop to loop throughout the string
+        for(int i = 0; i < str.length(); i++){
+            if (Character.isDigit(str.charAt(i))) {
+                // Checks for numbers
+                if(i < str.length() - 1 && Character.isDigit(str.charAt(i + 1))) {
+                    strNum += str.charAt(i);
+                }
+
+                else{
+                    strNum += str.charAt(i);
+                    intSum += Integer.parseInt(strNum);
+                    strNum = "";
+                }
+            }
+        }
+
+        return intSum;
+    }
     
+    /**
+     * Given a file, return the word that is alphabetically first
+     * 
+     * @param filenametxt String of file path name to determine word alpabetically first
+     * @return first word (alphabetical) in the array list at index 0
+     * @author B. Zhang
+     */
+    public static String alphaWord(String filenametxt) throws IOException {
+        BufferedReader Br = new BufferedReader(new FileReader(filenametxt));
+        ArrayList <String> strList = new ArrayList <String> ();
+        String strLine = Br.readLine();
+        int intNumLines = 0;
+        String strTemp;
+
+        // While loop to add all strings to an array list
+        while(strLine != null){
+            strList.add(strLine);
+            strLine = Br.readLine();
+            intNumLines ++;
+        }
+        Br.close();
+
+        // Bubble sorting alphabetically
+        for (int i = 0; i < intNumLines; i++) {
+            for (int j = i + 1; j < intNumLines; j++) {
+               
+                // Comparing string with other strings
+                if (strList.get(i).compareTo(strList.get(j)) > 0) {
+                    // Swaps
+                    strTemp = strList.get(i);
+                    strList.set(i, strList.get(j));
+                    strList.set(j, strTemp);
+                }
+            }
+        }
+        return strList.get(0);
+    }
+
+    /**
+     * Given an array and a value, return the array with larger numbers next to the values
+     * 
+     * @param nums Integer array given to determine number sequence
+     * @param val Integer given to select a specific element   
+     * @return nums (array of numbers) back to user
+     * @author B. Zhang
+     */
+    public static int[] notAlone(int[] nums, int val){
+        // For loop to loop through index numbers
+        for(int i = 0; i < nums.length - 1; i++){
+            if(nums[i] == val){
+                if(nums[i - 1] != nums[i] && nums[i + 1] != nums[i]){
+                    // Checks and compares adjacent numbers
+                    if(nums[i + 1] > nums[i - 1]){
+                        nums[i] = nums[i + 1];
+                    }
+                    else if(nums[i + 1] < nums[i - 1]){
+                        nums[i] = nums[i - 1];
+                    }
+                }
+            }
+        }
+        return nums;
+    }
+
+    /**
+     * Given an array, determine if the array is balanced (even)
+     * 
+     * @param nums Integer array given to determine balance
+     * @return true back to user if sum is even
+     * @return false back to user if sum is odd
+     * @author B. Zhang
+     */
+    public static boolean canBalance(int[] nums){
+        int intSum = 0;
+        // For loop to add all numbers in the array 
+        for(int i = 0; i < nums.length; i++){
+            intSum += nums[i];
+        }
+        
+        // Checks if the sum is odd or even
+        if(intSum % 2 == 0){
+            return true;
+        }
+        else{
+            return false;
+        }
+    }
+
+    /**
+     * Given a 2d array, row, and column, return an array with all of the numbers up to the values given.
+     * 
+     * @param arr 2d array given to 
+     * @param row Number of rows given for arrays
+     * @param col Number of columns given for arrays
+     * @return the 2d array intNum 
+     * @author B. Zhang
+     */
+    public static int[][] split(int[][] arr, int row, int col){
+        int[][] intNum = new int[row + 1][col + 1];
+        // Nested for loop to assign the element from intNum with arr
+        for(int i = 0; i < row + 1; i++){
+            for(int j = 0; j < col + 1; j++){
+                intNum[i][j] = arr[i][j];
+            }
+        }
+        return intNum;
+    }
 }
--- src/gr11review/part2/UtilityTest.java
+++ src/gr11review/part2/UtilityTest.java
@@ -1,10 +1,136 @@
 package gr11review.part2;
 
 import static org.junit.jupiter.api.Assertions.*;
+
+import java.io.IOException;
+import java.util.Arrays;
+
 import org.junit.jupiter.api.Test;
 
-import gr11review.part2.Utility;
-
 public class UtilityTest{
+
+    // 2d array test cases and solutions
+    int [][] splitCase1 = {
+        {1, 2, 3},
+        {4, 5, 6},
+        {7, 8, 9},
+    };
+
+    int [][] splitSol1 = {
+        {1, 2, 3},
+        {4, 5, 6},
+        {7, 8, 9}
+    };
+
+    int [][] splitCase2 = {
+        {1, 2, 3},
+        {4, 5, 6},
+        {7, 8, 9}
+    };
+
+    int [][] splitSol2 = {
+        {1, 2, 3},
+        {4, 5, 6}
+    };
   
+    // simple test
+    @Test
+    void sumNumbersTest1() {
+        assertEquals(1, Utility.sumNumbers("a1"));
+    }
+
+    // testing single char to return 0
+    @Test
+    void sumNumbersTest2() {
+        assertEquals(0, Utility.sumNumbers("a"));
+    }
+
+    // testing single integer
+    @Test
+    void sumNumbersTest3() {
+        assertEquals(1, Utility.sumNumbers("1"));
+    }
+
+    // simple test
+    @Test
+    void alphaWordTest1() throws IOException{
+        assertEquals("accompany", Utility.alphaWord("C:\\Users\\brigh\\grade-11-review-2---methods-and-testing-james-brighton-hayden-1\\src\\gr11review\\test2\\Review2_3Test_1.txt"));
+    }
+
+    // testing for single char
+    @Test
+    void alphaWordTest2() throws IOException{
+        assertEquals("x", Utility.alphaWord("C:\\Users\\brigh\\grade-11-review-2---methods-and-testing-james-brighton-hayden-1\\src\\gr11review\\test2\\Review2_3Test_2.txt"));
+    }
+
+    // simple test
+    @Test
+    void alphaWordTest3() throws IOException{
+        assertEquals("accompanyx", Utility.alphaWord("C:\\Users\\brigh\\grade-11-review-2---methods-and-testing-james-brighton-hayden-1\\src\\gr11review\\test2\\Review2_3Test_3.txt"));
+    }
+
+    // simple test
+    @Test
+    void alphaWordTest4() throws IOException{
+        assertEquals("accompany", Utility.alphaWord("C:\\Users\\brigh\\grade-11-review-2---methods-and-testing-james-brighton-hayden-1\\src\\gr11review\\test2\\Review2_3Test_4.txt"));
+    }
+
+    // simple test
+    @Test
+    void notAlongTest1(){
+        int[] inArray = {1, 2, 3};
+        int[] outArray = {1, 3, 3};
+
+        assertTrue(Arrays.equals(Utility.notAlone(inArray,2), outArray) );
+    }
+
+    // simple test
+    @Test
+    void notAloneTest2(){
+        int[] inArray = {1, 2, 3, 2, 5, 2};
+        int[] outArray = {1, 3, 3, 5, 5, 2};
+
+        assertTrue(Arrays.equals(Utility.notAlone(inArray,2), outArray) );
+    }
+
+    // simple test (even)
+    @Test
+    void canBalanceTest1() throws IOException{
+        assertEquals(true, Utility.canBalance(new int[] {1, 1, 1, 2, 1}));
+
+    }
+
+    // simple test (odd)
+    @Test
+    void canBalanceTest2() throws IOException{
+        assertEquals(false, Utility.canBalance(new int[] {2, 1, 1, 2, 1}));
+
+    }
+
+    // tests for two integers
+    @Test
+    void canBalanceTest3() throws IOException{
+        assertEquals(true, Utility.canBalance(new int[] {10, 10}));
+
+    }
+
+    // simple test
+    @Test
+    void splitTest1() {
+        int intIndex = splitCase1.length - 1;
+        int[][] resultArr = Utility.split(splitCase1, 2, 2);
+        for (int i = 0; i < intIndex; i++) {
+            assertArrayEquals(splitSol1[i], resultArr[i]);
+        }
+    }
+
+    // tests for different row and column numbers
+    @Test
+    void splitTest2() {
+        int intIndex = splitCase2.length - 1;
+        int[][] resultArr = Utility.split(splitCase2, 1, 2);
+        for (int i = 0; i < intIndex; i++) {
+            assertArrayEquals(splitSol2[i], resultArr[i]);
+        }
+    }
 }
